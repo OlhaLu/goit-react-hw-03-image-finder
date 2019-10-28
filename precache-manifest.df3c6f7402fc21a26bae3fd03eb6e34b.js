5fbf87a34d5320321e59acd6e6b402d8434d2277
OlhaLu <olhalutsyshyna@gmail.com>
2019-10-28 17:23:59 +0200

    Updates

---
+++ precache-manifest.df3c6f7402fc21a26bae3fd03eb6e34b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a5305f22a8663dee790303c31aa0c2c",
+    "url": "/goit-react-hw-03-image-finder/index.html"
+  },
+  {
+    "revision": "abd626d72a161e5cbf04",
+    "url": "/goit-react-hw-03-image-finder/static/css/main.7b63e0fe.chunk.css"
+  },
+  {
+    "revision": "5f497c670d125c041f7d",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.e7ee41b4.chunk.js"
+  },
+  {
+    "revision": "abd626d72a161e5cbf04",
+    "url": "/goit-react-hw-03-image-finder/static/js/main.25fb9ec4.chunk.js"
+  },
+  {
+    "revision": "26f980cacd962de52565",
+    "url": "/goit-react-hw-03-image-finder/static/js/runtime-main.9d9945f4.js"
+  }
+]);
